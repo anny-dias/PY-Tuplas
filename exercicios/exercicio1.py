22e1520665de8c640c927db0bb0bfee3bbcd8c9b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-18 11:38:40 -0300

    py

--- exercicios/exercicio1.py
+++ exercicios/exercicio1.py
@@ -1,2 +1,8 @@
 '''Exercícios  Strings, Listas e Tuplas
 1. Escreva uma função que receba uma string e retorne o número de caracteres nela.'''
+
+def conta_caracteres(frase):
+    return len(frase)
+
+frase = input('Digite a frase: ')
+print(conta_caracteres(frase))
--- exercicios/exercicio2.py
+++ exercicios/exercicio2.py
@@ -1,3 +1,9 @@
 '''2. Crie uma função que receba uma string e retorne a mesma string com todas as letras em 
 maiúsculas.
 '''
+
+def converte_maiusculo(frase):
+    return frase.upper()
+
+frase = input('Digite a frase: ')
+print(converte_maiusculo(frase))
